abd2167ab5a558c97d735721c7345ba5ee0f3043
zhangzidan <zhangzidan@huya.com>
2023-03-28 14:01:58 +0800

    feat: update

---
+++ scripts/rollup/utils.js
@@ -0,0 +1,29 @@
+import path from 'path'
+import fs from 'fs'
+import ts from 'rollup-plugin-typescript2'
+import csj from '@rollup/plugin-commonjs'
+
+const pkgPath = path.resolve(__dirname, '../../packages')
+const distPath = path.resolve(__dirname, '../../dist/node_modules')
+
+export function resolvePkgPath(pkgName, isDist) {
+    if(isDist) {
+        return `${distPath}/${pkgName}`
+    } else {
+        return `${pkgPath}/${pkgName}`
+    }
+}
+
+export function getPackageJSON (pkgName) {
+    // 包路径
+    const path = `${resolvePkgPath(pkgName)}/package.json`
+    const str = fs.readFileSync(path, {  encoding: 'utf-8' })
+    return JSON.parse(str)
+}
+
+export function getBaseRollupPlugins({typescript = {}} = {}) {
+    return [
+        csj(),
+        ts(typescript)
+    ]
+}
