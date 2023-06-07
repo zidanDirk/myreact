35d79306b8c984861bb8ed834b8d05f4ea7fd8b2
zhangzidan <zhangzidan@huya.com>
2023-06-07 17:32:25 +0800

    feat: {9} finish

--- packages/react-reconciler/src/completeWork.ts
+++ packages/react-reconciler/src/completeWork.ts
@@ -1,14 +1,12 @@
-import { FiberNode } from "./fiber";
+import { FiberNode } from './fiber';
 
 // 递归中的归阶段
 export const completeWork = (wip: FiberNode) => {
-	
 	// const newProps = wip.pendingProps
 	// const current = wip.alternate
 
 	// switch(wip.tag) {
-		
-	// }
-	console.log(`completeWork`)
 
+	// }
+	console.log(`completeWork`);
 };
